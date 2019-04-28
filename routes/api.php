b182b4ad34369696adf3659f8124bfc3633ec6f2
William R <wiribasczky@gmail.com>
2019-04-28 02:57:56 -0300

    Configurado novo middleware para autorefresh token
    -Configurado um novo middleware no Kernel.php para auto refresh
    -Adicionado o middleware no grupo de rotas protegidas

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::group(['namespace'=>'Api','as'=>'api.'],function(){
     Route::name('refresh')->post('refresh','AuthController@refresh');
 
 
-    Route::group(['middleware'=>'auth:api'],function(){
+    Route::group(['middleware'=>['auth:api','jwt.refresh']],function(){
         Route::name('logout')->post('logout','AuthController@logout');
         Route::name('me')->get('me',"AuthController@me");
         Route::patch('products/{product}/restore','ProductController@restore');
