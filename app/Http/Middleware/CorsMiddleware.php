ffc308872d77aa25c57508278aa6088623a758c2
William R <wiribasczky@gmail.com>
2019-05-09 19:48:46 -0300

    Configuado CORS paar expor header Authorization
    -Configurado CORS para liberar acesso ao HEADER Authorization,
    pois o refresh token é devolvido por este HEADER.

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -19,6 +19,7 @@ class CorsMiddleware
             header('Access-Control-Allow-Origin:*');
             header('Access-Control-Allow-Headers:Content-Type, Authorization');
             header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
+            header('Access-Control-Expose-Headers: Authorization');
         }
         return $next($request);
     }
