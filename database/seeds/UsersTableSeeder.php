2e76558d14a0485af2685da171da65398c246b7f
William R <wiribasczky@gmail.com>
2019-05-19 06:34:39 -0300

    Criado model para perfil de usuario
    -Criado model para perfil de usuario, onde tera
    dados especificos, como numero de telefone e etc.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,7 +14,11 @@ class UsersTableSeeder extends Seeder
     {
         factory(User::class,1)->create([
             'email'=>'admin@user.com'
-        ]);
+        ])->each(function($user){
+            $user->profile->phone_number = '+166505551234';
+            $user->profile->save();
+        });
+
         factory(User::class,50)->create();
     }
 }
