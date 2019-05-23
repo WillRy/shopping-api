96fda2f43f75bc8c09a81ffde40917e95d750cc6
William R <wiribasczky@gmail.com>
2019-05-23 17:11:31 -0300

    Request de customerUpdate limpa de comentarios

--- app/Http/Requests/ChatGroupUpdateRequest.php
+++ app/Http/Requests/ChatGroupUpdateRequest.php
@@ -2,36 +2,23 @@
 
 namespace CodeShopping\Http\Requests;
 
-use CodeShopping\Rules\PhoneNumberUnique;
-use Illuminate\Foundation\Http\FormRequest;
-use CodeShopping\Rules\FirebaseTokenVerification;
+use CodeShopping\Http\Requests\ChatGroupCreateRequest;
+
+
 
 class ChatGroupUpdateRequest extends ChatGroupCreateRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
         return true;
     }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         $rules = parent::rules();
         $this->removeRulesRequiredFromPhoto($rules);
         return $rules;
     }
-
-    private function removeRulesRequiredFromPhoto(array &$rules)
-    {
+    private function removeRulesRequiredFromPhoto(array &$rules){
         $rules['photo'] = str_replace('required|', '', $rules['photo']);
     }
 }
