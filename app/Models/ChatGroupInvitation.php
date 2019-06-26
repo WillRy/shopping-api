d508b6c96b7c6da756537ec0cc5ceb8e05fc6c6d
William R <wiribasczky@gmail.com>
2019-06-26 15:25:39 -0300

    Correção da Model ChatGroupInvitation
    Correção do método hasInvitation, pois a verificação 
    da data de expiração estava incorreta, devido a má 
    formatação da data.

--- app/Models/ChatGroupInvitation.php
+++ app/Models/ChatGroupInvitation.php
@@ -12,11 +12,15 @@ class ChatGroupInvitation extends Model
     use SoftDeletes;
 
     protected $fillable = ['total', 'expires_at', 'group_id'];
-    protected $dates = ['expires_at', 'deleted_at'];
+    protected $dates = ['expires_at', 'deleted_at']; // Carbon --> adiciona 00:00:00
 
     public function hasInvitation()
     {
-        return $this->remaining > 0 && (!$this->expires_at or (new Carbon())->lessThanOrEqualTo("{$this->expires_at} 23:59:59"));
+        $expiresAt = $this->expires_at;
+        $expiresAt->hour(23);
+        $expiresAt->minute(59);
+        $expiresAt->second(59);
+        return $this->remaining > 0 && (!$this->expires_at or (new Carbon())->lessThanOrEqualTo($expiresAt));
     }
 
     public function group()
