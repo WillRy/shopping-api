1c567bd38d49064bb936dc6ef71f475e0e6abd02
William R <wiribasczky@gmail.com>
2019-07-12 09:25:47 -0300

    Criado filter para busca de pedidos
    Criado filter responsável por buscas personalizadas de pedidos

--- app/Http/Filters/OrderFilter.php
+++ app/Http/Filters/OrderFilter.php
@@ -6,5 +6,35 @@ use Mnabialek\LaravelEloquentFilter\Filters\SimpleQueryFilter;
 
 class OrderFilter extends SimpleQueryFilter
 {
-    protected $simpleSorts = ['id','amount', 'price', 'created_at'];
+    protected $simpleFilters = ['search'];
+
+    protected $simpleSorts = ['id', 'total', 'created_at', 'user', 'product'];
+
+    protected function applySearch($value)
+    {
+        $this->query
+            ->where('users.name', 'LIKE', "%$value%")
+            ->orWhere('products.name', 'LIKE', "%$value%");
+    }
+
+    protected function applySortUser($order){
+        $this->query->orderBy('users.name', $order);
+    }
+
+    protected function applySortProduct($order){
+        $this->query->orderBy('products.name', $order);
+    }
+
+    /**
+     * @param Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply($query)
+    {
+        $query = $query
+            ->select('orders.*')
+            ->join('products', 'products.id', '=', 'orders.product_id')
+            ->join('users', 'users.id', '=', 'orders.user_id');
+        return parent::apply($query);
+    }
 }
